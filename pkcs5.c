b4294fae7426cac1cecaf27462030cf0468d4c8e
vlinx <vlinx@vlinx.io>
2019-04-05 18:20:12 +0800

    Update pkcs5 way

--- pkcs5.c
+++ pkcs5.c
@@ -20,12 +20,8 @@ uint8_t* PKCS5Padding(uint8_t* data, uint32_t length,uint32_t* padded_len)
   return result;
 }
 
-uint8_t* InvPKCS5Padding(uint8_t* data, uint32_t length,uint32_t* unpadded_len)
+void InvPKCS5Padding(uint8_t* data, uint32_t length,uint32_t* unpadded_len)
 {
 	uint8_t padding = data[length-1];
 	*unpadded_len = length-padding;
-	uint8_t* result = malloc(*unpadded_len*sizeof(uint8_t));
-	memcpy(result,data,*unpadded_len);
-	return result;
-
 }
